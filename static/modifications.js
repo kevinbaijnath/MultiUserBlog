c05f609fe5bc9e155ca33351b3b9fe13ce18c45d
Kevin Baijnath <kevinbaijnath@gmail.com>
2016-12-07 17:13:59 -0500

    updating js (in progress)

--- static/modifications.js
+++ static/modifications.js
@@ -4,23 +4,50 @@ $( document ).ready(function() {
             $("#error").slideUp(1000);
         });
     }
-    $("#showCommentForm").click(function(){
-        if($("#newCommentBox").is(':hidden')){
-            $("#newCommentBox").show();
-            $("#showCommentForm").text("Cancel");
-        }else{
-            $("#newCommentBox").hide();
-            $("#showCommentForm").text("Comment");
-        }
+    $(".post").find(".show-new-comment-form").click(function(){
+        item = $(".post")
+        item.find(".show-new-comment-form").click(function(){
+            if (item.find(".new-comment-box").is(':hidden')){
+                item.find(".new-comment-box").show();
+                item.find(".show-new-comment-form").text("Cancel")
+            }else{
+                item.find(".new-comment-box").hide();
+                item.find(".show-new-comment-form").text("Comment")
+            }
+        })
     });
-    $("#showComments").click(function(){
-        $("#hideComments").show()
-        $("#commentBox").show()
-        $("#showComments").hide()
+
+    $(".post").find(".show-comments").click(function(){
+        item = $(".post")
+        item.find(".hide-comments").show()
+        item.find(".comment-box").show()
+        item.find(".show-comments").hide()
     });
-    $("#hideComments").click(function(){
-        $("#showComments").show()
-        $("#commentBox").hide()
-        $("#hideComments").hide()
+
+    $(".post").find(".hide-comments").click(function(){
+        item = $(".post")
+        item.find(".show-comments").show()
+        item.find(".comment-box").hide()
+        item.find(".hide-comments").hide()
     });
+
+    // $("#showCommentForm").click(function(){
+    //     if($("#newCommentBox").is(':hidden')){
+    //         $("#newCommentBox").show();
+    //         $("#showCommentForm").text("Cancel");
+    //     }else{
+    //         $("#newCommentBox").hide();
+    //         $("#showCommentForm").text("Comment");
+    //     }
+    // });
+    // $("#showComments").click(function(){
+    //     $("#hideComments").show()
+    //     $("#commentBox").show()
+    //     $("#showComments").hide()
+    // });
+    // $("#hideComments").click(function(){
+    //     $("#showComments").show()
+    //     $("#commentBox").hide()
+    //     $("#hideComments").hide()
+    // });
 });

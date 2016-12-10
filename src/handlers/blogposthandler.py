a63be232b8fc40c377b136b3ee3c15b55f1aee24
Kevin Baijnath <kevinbaijnath@gmail.com>
2016-12-10 10:40:39 -0500

    Fixed bug where blog post creator name wasn't being passed if a user wasn't logged in

--- src/handlers/blogposthandler.py
+++ src/handlers/blogposthandler.py
@@ -1,39 +1,34 @@
+from ..constants import BLOG_KEY, COMMENT_KEY
 from ..handlers.bloghandler import BlogHandler
+from ..helpers.decorators import blogpost_exists
 from ..models.blogpost import BlogPost
 from ..models.comment import Comment
 from ..models.user import User
-from ..constants import BLOG_KEY, COMMENT_KEY
 
 
 class BlogPostHandler(BlogHandler):
     """Blog Post Page"""
 
+    @blogpost_exists
     def get(self, blog_id):
         """Renders a Blog Post Page based on the ID"""
-        if not blog_id.isdigit():
-            self.error(404)
-            self.render("404.html")
-
         error = self.request.get("error")
         blog_post = BlogPost.get_by_id(int(blog_id), parent=BLOG_KEY)
-        if blog_post:
-            comments = Comment.get_by_id(blog_post.comment_ids,
-                                         parent=COMMENT_KEY)
-            if self.user:
-                username = [User.get_by_id(blog_post.user_id).username]
-                self.render("blog_post.html",
-                            blog_posts=[blog_post],
-                            comments=[comments],
-                            error=error,
-                            user_id=self.user.key().id(),
-                            creators=username,
-                            liked_posts=self.user.liked_posts)
-            else:
-                self.render("blog_post.html",
-                            blog_posts=[blog_post],
-                            comments=[comments],
-                            user_id=None,
-                            error=error)
+        comments = Comment.get_by_id(blog_post.comment_ids, parent=COMMENT_KEY)
+
+        username = [User.get_by_id(blog_post.user_id).username]
+        if self.user:
+            self.render("blog_post.html",
+                        blog_posts=[blog_post],
+                        comments=[comments],
+                        error=error,
+                        user_id=self.user.key().id(),
+                        creators=username,
+                        liked_posts=self.user.liked_posts)
         else:
-            self.error(404)
-            self.render("404.html")
+            self.render("blog_post.html",
+                        blog_posts=[blog_post],
+                        comments=[comments],
+                        creators=username,
+                        user_id=None,
+                        error=error)
